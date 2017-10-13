a4c1ac7c37759226f041218fa7cc404b00bcbda0
David Ng <nwingt@gmail.com>
2017-10-13 14:43:38 +0800

    ! Fix unable to create story

--- modmod/views/story.py
+++ modmod/views/story.py
@@ -145,8 +145,9 @@ def add_story(request):
 
         story_name = '{} {}'.format(story_name, len(user.stories) + 1)
 
-        story = Story(name=story_name, \
-            language=user.language)
+        story = Story.create(DBSession, name=story_name)
+
+        story.language = user.language
 
         user.stories.append(story)
 
