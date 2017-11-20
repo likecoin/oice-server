6e03c057d2f331184cf463314a8944850a215166
Ng Wing Tat (David) <i@ngwingt.at>
2017-11-20 17:40:33 +0800

    ! Fix characterdialog on toggling dim and/or move when dismissing

--- modmod/operations/character_script_data.py
+++ modmod/operations/character_script_data.py
@@ -1,19 +1,19 @@
 # pylama:ignore=E501
 
 fg_show = {
-    "left": '''@fgLeft storage="%(fg)s" left="&ch['%(key)s'].xl" top="&ch['%(key)s'].yl" move="%(move)s" dim="%(dim)s"''',
-    "right": '''@fgRight storage="%(fg)s" left="&ch['%(key)s'].xr" top="&ch['%(key)s'].yr" move="%(move)s" dim="%(dim)s"''',
-    "middle": '''@fgMiddle storage="%(fg)s" left="&ch['%(key)s'].xm" top="&ch['%(key)s'].ym"'''
+    "left": '''@fgLeft key="%(key)s" storage="%(fg)s" left="&ch['%(key)s'].xl" top="&ch['%(key)s'].yl" move="%(move)s" dim="%(dim)s"''',
+    "right": '''@fgRight key="%(key)s" storage="%(fg)s" left="&ch['%(key)s'].xr" top="&ch['%(key)s'].yr" move="%(move)s" dim="%(dim)s"''',
+    "middle": '''@fgMiddle key="%(key)s" storage="%(fg)s" left="&ch['%(key)s'].xm" top="&ch['%(key)s'].ym"'''
 }
 
 fg_to_bright = {
-    "right": """@fgRightToBright left="&ch['%(key)s'].xDr" top="&ch['%(key)s'].yDr" storage="%(fg)s" path="&ch['%(key)s'].pathr\"""",
-    "left": """@fgLeftToBright storage="%(fg)s" left="&ch['%(key)s'].xDl" top="&ch['%(key)s'].yDl" path="&ch['%(key)s'].pathl\""""
+    "right": """@fgRightToBright key="%(key)s" left="&ch['%(key)s'].xDr" top="&ch['%(key)s'].yDr" storage="%(fg)s" path="&ch['%(key)s'].pathr\"""",
+    "left": """@fgLeftToBright key="%(key)s" storage="%(fg)s" left="&ch['%(key)s'].xDl" top="&ch['%(key)s'].yDl" path="&ch['%(key)s'].pathl\""""
 }
 
 fg_to_dark = {
-    "right": """@fgRightToDark left="&ch['%(key)s'].xr" top="&ch['%(key)s'].yr" storage="%(fg)s" path="&ch['%(key)s'].pathDr\"""",
-    "left": """@fgLeftToDark left="&ch['%(key)s'].xl" top="&ch['%(key)s'].yl" storage="%(fg)s" path="&ch['%(key)s'].pathDl\""""
+    "right": """@fgRightToDark key="%(key)s" left="&ch['%(key)s'].xr" top="&ch['%(key)s'].yr" storage="%(fg)s" path="&ch['%(key)s'].pathDr\"""",
+    "left": """@fgLeftToDark key="%(key)s" left="&ch['%(key)s'].xl" top="&ch['%(key)s'].yl" storage="%(fg)s" path="&ch['%(key)s'].pathDl\""""
 }
 
 fg_exit = {
